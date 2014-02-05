92a9303599d272e08eab366a3d4b69808a08ecb4
kevdawg <kevin@lanilabs.com>
2014-02-05 14:53:26 -0500

    commiting all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaniMobile::Application.config.secret_key_base = '39e8d544283ec2ebccb82542b72ede28fb6f68ae22b6de1688b9b8e17c0cf6341e22b244ce11211d0a02eb9df55f502351073b73694cab9db850d87446ffa030'
